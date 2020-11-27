012b248faffb5d5f237117125077839f95dbdede
Netta Rutenberg <nettar@wix.com>
2020-11-27 11:48:00 +0100

    add notifications on high load and recovery events

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,6 +2,7 @@ import '../styles/index.css';
 import Store from './store';
 import Page from './ui/page';
 import SocketManager from './socket-manager';
+import NotificationsManager from './notifications-manager';
 
 export function boot () {
   // Create new state store
@@ -10,6 +11,9 @@ export function boot () {
   // Create socket manager to handle communication with server
   SocketManager(store);
 
+  // Create notification manager to show alerts on high cpu load events
+  NotificationsManager(store);
+  
   // Create the UI and append to the document
   const page = new Page(store);
   document.getElementById('root').append(page.render());
---
+++ app/scripts/notifications-manager.js
@@ -0,0 +1,38 @@
+import { ACTION_TYPES } from './config';
+
+function getLatestEventLength (events) {
+  const latestEvent = events[events.length - 1];
+  return latestEvent ? (latestEvent.end - latestEvent.start)/60000 : 0;
+}
+
+/**
+ * Notifies user in cases of high CPU load events and system recovery
+ * 
+ * Uses the notifications API if supported by the browser and approved by the user,
+ * Otherwise - uses window.alert
+ * @param {object} store 
+ */
+export default function NotificationsManager (store) {
+  let notify = (title, body) => alert(`${title}\n${body}`);
+  if (window.Notfication && Notification.permission !== 'granted') {
+    Notification.requestPermission()
+      .then(permission => {
+        Notification.permission = permission;
+        if (permission == 'granted') {
+          notify = (title, body) => { new Notification(title, { body }); }
+        }
+      });
+  } else if (window.Notfication && Notification.permission == 'granted') {
+    notify = (title, body) => { new Notification(title, { body }); }
+  }
+
+  store.subscribe(eventTypes => {
+    const highCPULoad = store.getState().highCPULoad;
+
+    if (eventTypes.includes(ACTION_TYPES.HIGH_CPU_LOAD)) {
+      notify('High CPU Load!', `Started ${getLatestEventLength(highCPULoad.events)} minutes ago`);
+    } else if (eventTypes.includes(ACTION_TYPES.NORMAL_CPU_LOAD)) {
+      notify('CPU Load Recovered', `Event length was: ${getLatestEventLength(highCPULoad.events)} minutes`);
+    }
+  });
+};
